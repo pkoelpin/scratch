25b55383d23bec0c5b3e1476cfbcfa69487093ab
Phillip Koelpin <phil.koelpin@gmail.com>
2023-10-06 09:07:06 -0700

    update

--- potato/potato.c
+++ potato/potato.c
@@ -1,5 +1,6 @@
 /* potato - reduce data to enveloping potato plot */
 /* Written by Phil Koelpin */
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stddef.h>
 #include <stdio.h>
@@ -8,11 +9,82 @@
 #include <assert.h>
 #include <math.h>
 #include <stdbool.h>
-#include "getopt.h"
+#include <stdint.h>
+#include <inttypes.h>
+#include <ctype.h>
 
-#define PROGRAM_NAME "potato"
+#define OPTPARSE_IMPLEMENTATION
+#include "optparse.h"
 
 static struct point *points;
+struct pair *combo;
+static int n_combo;
+static size_t n_combo_allocated;
+static unsigned char delimeter;
+static bool merge;
+static int sentinel;
+
+#define FATAL_ERROR(Message)            \
+    do                                  \
+    {                                   \
+        fprintf(stderr, Message);       \
+        exit(EXIT_FAILURE);             \
+    }                                   \
+    while (0)
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+
+struct pair {
+    uint64_t lo;
+    uint64_t hi;
+};
+
+const char * getrange(uint64_t *lo, uint64_t *hi, const char *str) {
+    uint64_t initial = 0;	
+    uint64_t value = 0;
+    bool lhs_specified = false;
+    bool rhs_specified = false;
+    bool dash_found = false;
+
+    /* consume whitespace */
+    while (isspace(*str))
+        str++;
+
+    for (;;) {
+        if (*str == '-') {
+            if (dash_found) {
+                FATAL_ERROR("invalid range. Too many dashes.");
+            }
+            dash_found = true;
+            initial = value;
+            value = 0;
+            str++;
+        } else if (*str == ',' || isspace(*str) || *str == '\0' ) {
+            if (dash_found) {
+                dash_found = false;
+                if (!lhs_specified || !rhs_specified)
+                    FATAL_ERROR("invalid range.");
+                if (value < initial)
+                    FATAL_ERROR("decreasing range.");
+                *lo = initial; 
+                *hi = value;
+            } else {
+                *lo = value; 
+                *hi = value; 
+            }
+            return *str == '\0' ? str : ++str;
+        } else if (isdigit(*str)) {
+            if (dash_found)
+                rhs_specified = true;
+            else
+                lhs_specified = true;
+            value = value*10 + *str - '0';
+            str++;
+        } else {
+            FATAL_ERROR("invalid character in range.");
+        }
+    }
+}
 
 size_t xgetdelim(char **lineptr, size_t *n, int delim, FILE *stream) {
     int c;
@@ -24,7 +96,7 @@ size_t xgetdelim(char **lineptr, size_t *n, int delim, FILE *stream) {
         if (len)
             line[0] = 0;
         len = 256;
-        if (!(line = realloc(line, len)))
+        if ((line = realloc(line, len)) == NULL)
             return 0;
         *lineptr = line;
         *n = len;
@@ -36,7 +108,7 @@ size_t xgetdelim(char **lineptr, size_t *n, int delim, FILE *stream) {
             line[nread] = 0;
             if (len * 2 < len)
                 return 0;
-            if (!(line = realloc(line, len *= 2)))
+            if ((line = realloc(line, len *= 2)) == NULL)
                 return 0;
             *lineptr = line;
             *n = len;
@@ -47,7 +119,7 @@ size_t xgetdelim(char **lineptr, size_t *n, int delim, FILE *stream) {
             line[nread] = 0;
             return nread;
         }
-        line[nread++] = c;
+        line[nread++] = (char)c;
         if (c == delim) {
             line[nread] = 0;
             return nread;
@@ -69,6 +141,9 @@ struct point {
 };
 
 void point_delete(struct point *p) {
+    if (p == NULL) {
+        return;
+    }
     if (p->next != NULL) {
         p->next->prev = p->prev;
     }
@@ -234,37 +309,150 @@ struct node* insert(struct node *head, struct point *p) {
     }
 }
 
-bool potato_file(char const *file) {
-    
+void add_combo(struct pair p) {
+    if (n_combo == n_combo_allocated) {
+        n_combo_allocated *= 2;
+        combo = realloc(combo, n_combo_allocated * sizeof (*combo));
+    }
 
+    for (int i = 0; i < n_combo; i++) {
+        if (p.lo == combo[i].lo && p.hi == combo[i].hi)
+            return;
+    }
+
+    combo[n_combo++] = p;
+}
+
+void set_combos(const char *str) {
+    uint64_t lo = 0;
+    uint64_t hi = 0;
+    int n_range = 0;
+    int n_range_allocated = 1;
+    struct pair *range = malloc(n_range_allocated*sizeof(*range));
+
+    while (*str != '\0') {
+        str = getrange(&lo, &hi, str);
+        
+        /* add more space if we need it */
+        if (n_range+1 == n_range_allocated) {
+            n_range_allocated *= 2;
+            range = realloc(range, n_range_allocated *sizeof(*range));
+        }
+
+        /* insert the range into the list of ranges */
+        bool found = false;
+        for (int i = 0; i < n_range; i++){
+            if (lo <= range[i].lo) {
+                memmove(range + i + 1, range + i, (n_range - i)*sizeof(*range));
+                range[i] = (struct pair){lo, hi};
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            range[n_range] = (struct pair){lo, hi};
+        }
+        n_range++;
+
+        /* reduce any overlapping ranges */
+        for (int i = 0; i < n_range; i++){   
+            for (int j= i + 1; j < n_range; j++) {
+                if (range[j].lo <= range[i].hi) {
+                    range[i].hi = MAX(range[j].hi, range[i].hi);
+                    memmove(range + j, range + j + 1, (n_range - j - 1)*sizeof *range);
+                    n_range--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    /* generate the combinations */
+    for (int i = 0; i < n_range; i++) {
+        for (uint64_t v1 = range[i].lo; v1 <= range[i].hi; v1++) {
+            for (uint64_t v2 = v1 + 1; v2 <= range[i].hi; v2++)
+                add_combo((struct pair){v1, v2});
+            for (int j = i + 1; j < n_range; j++) {
+                for (uint64_t v2 = range[j].lo; v2 <= range[j].hi; v2++)
+                    add_combo((struct pair){v1, v2});
+            }
+        }
+    } 
+    free(range);
+}
+
+void potato_file(char const *file) {
+    FILE *stream;
+
+    if (strcmp(file, "-") == 0) {
+        stream = stdin;
+    } else {
+        stream = fopen(file, "r");
+        if (stream == NULL) {
+            FATAL_ERROR("could not open file\n");
+        }
+    }
+
+    potato_stream(stream);
+
+    if (strcmp(file, "-") == 0) {
+        clearerr(stream);
+    } else {
+        fclose(stream);
+    }
 }
 
 int main(int argc, char **argv) {
-   
-    bool have_read_stdin = false;
+    (void)argc;
+    n_combo = 0;
+    n_combo_allocated = 1;
+    combo = malloc(n_combo_allocated*sizeof(*combo));
+    delimeter = '\t';
+    merge = false;
 
     /* argument parsing */
-    int optc = 0;
-    optind = 1;
-    while ((optc = getopt(argc, argv, "d:f:h")) != -1) {
-        printf("%d\n", optind);
-        printf("%c\n", optopt);
-        printf("%s\n", optarg);
-        printf("---\n");
+     struct optparse_long longopts[] = {
+        {"delimeter", 'd', OPTPARSE_REQUIRED},
+        {"fields", 'f', OPTPARSE_REQUIRED},
+        {"merge", 'm', OPTPARSE_NONE},
+        {"sentinel", 's', OPTPARSE_REQUIRED},
+        {0}
+    };
+    int option;
+    struct optparse options;
+    optparse_init(&options, argv);
+    while ((option = optparse_long(&options, longopts, NULL)) != -1) {
+        switch (option) {
+        case 'd':
+            delimeter = options.optarg[0];
+            break;
+        case 'f':
+            set_combos(options.optarg);
+            break;
+        case 'm':
+            merge = true;
+            break;
+        case 's':
+            // sentinel = options.optarg;
+            break;
+        case '?':
+            fprintf(stderr, "%s: %s\n", argv[0], options.errmsg);
+            exit(EXIT_FAILURE);
+        }
     }
 
+    /* the remaining arguments are all files */
+    char *arg;
+    while ((arg = optparse_arg(&options)) != '\0')
+        printf("%s\n", arg);
 
-    for (;optind < argc; optind++) {
-        printf("%s\n", argv[optind]);
-    }
-    
-   
 
-   
     char *line = NULL;
     size_t n = 0;
     struct node *head = node_create(NULL);
-    int len = 0;
+    size_t len = 0;
     int index = 1;
     points = NULL;
     while ((len = xgetline(&line, &n, stdin)) > 0) {
@@ -276,12 +464,10 @@ int main(int argc, char **argv) {
         p->ref_count = 0;
         int count = sscanf(line, "%lf,%lf", &p->f[0], &p->f[1]);
         if (count < 2) {
-            fprintf(stderr, "not enough variables in line\n");
-            return 1;
+            FATAL_ERROR("not enough variables in line\n");
         }
         if (isinf(p->f[0]) || isinf(p->f[1])){
-            fprintf(stderr, "infinity in line\n");
-            return 1;
+            FATAL_ERROR("infinity in line\n");
         }
         if (points == NULL) {
             points = p;
